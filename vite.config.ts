274970c040df9ea5954b28c8089aeebe3d04224d
hcanning <46687863+hcanning@users.noreply.github.com>
2025-06-27 17:46:36 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  server: {
+    host: '::',
+    port: 8080,
+  },
   base: '/imdb-api1',
   plugins: [react(), mode === 'development' && componentTagger()].filter(
     Boolean
